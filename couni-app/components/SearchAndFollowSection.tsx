d5a77214de868a57692e121f18de46e6527b0549
LyghtCode <xikago.musica@gmail.com>
2022-02-28 21:33:13 -0600

    Import Co-Uni branding

--- couni-app/components/SearchAndFollowSection.tsx
+++ couni-app/components/SearchAndFollowSection.tsx
@@ -16,17 +16,17 @@ const SearchAndFollowSection = () => {
 
 	return (
 		<div className='w-4/12 text-white p-5'>
-			<input type="text" placeholder='Search Twitter' className='bg-gray-800 w-full p-3 rounded-full outline-none focus:outline-blue-600' />
+			<input type="text" placeholder='Search Co-Uni' className='bg-gray-800 w-full p-3 rounded-full outline-none focus:outline-blue-600' />
 			<div className='bg-gray-800 mt-5 rounded-xl'>
 				<h1 className='text-xl font-bold p-3'>People to follow</h1>
-				{peopleToFollow?.map((person: any) => 
-				<Link href={person.ethAddress} key={person.ethAddress}>
+				{/* {peopleToFollow?.map((person: any) => 
+				<Link href={person.ethAddress} key={person.ethAddress} passHref>
 					<div className='mt-3 hover:bg-gray-600 hover:cursor-pointer p-3'>
 						<h3 className='text-lg font-medium'>{person.username}</h3>
 						<p className='font-light text-gray-400'>{person.tweets?.length} tweets</p>
 					</div>
 				</Link>
-				)}
+				)} */}
 			</div>
 		</div>
 	)
